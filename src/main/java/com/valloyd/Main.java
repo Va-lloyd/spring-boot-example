88f552ee71c7ae4229ad30ad6c10a870b7b866d1
Va-lloyd <dyollajv@gmail.com>
2023-10-06 00:29:24 -0400

    Some config

--- src/main/java/com/valloyd/Main.java
+++ src/main/java/com/valloyd/Main.java
@@ -3,6 +3,7 @@ package com.valloyd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,9 +16,11 @@ public class Main {
 	}
 
 	@GetMapping("/greet")
-	public GreetResponse greet(){
+	public GreetResponse greet(
+			@RequestParam(value = "name", required = false) String name){
+		String greetMessage = name == null || name.isBlank() ? "Hello" : "Hello " + name;
 		GreetResponse response = new GreetResponse(
-				"Hello",
+				greetMessage,
 				List.of("Java", "Golang", "Javascript"),
 				new Person("Valloyd", 28, 30000)
 		);
